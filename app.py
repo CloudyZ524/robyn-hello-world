a299fba807204b750593528a131b5ee474426ffa
Cloudy <mac@Cloudy.local>
2024-07-10 23:59:29 -0700

    Update app.py

--- app.py
+++ app.py
@@ -9,5 +9,6 @@ def index():
     return "Hello World!"
 
 if __name__ == "__main__":
+    # Read the PORT environment variable for deployment
     port = int(os.getenv("PORT", 8080))
     app.start(host="0.0.0.0", port=port)
